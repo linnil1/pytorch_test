693b0a1705a7eac304e1abd9c50be2238ac675d9
linnil1 <linnil1.886@gmail.com>
2019-03-18 23:43:40 +0800

    add circle to density

---
+++ circle/build.py
@@ -0,0 +1,60 @@
+import h5py
+from PIL import Image, ImageDraw
+import numpy as np
+import os
+import glob
+from matplotlib import pyplot as plt
+from scipy.ndimage.filters import gaussian_filter 
+
+#this is borrowed from https://github.com/davideverona/deep-crowd-counting_crowdnet
+def gaussian_filter_density_fix(gt):
+    density = np.array(gt, dtype=np.float32)
+    sigma = 15
+    density = gaussian_filter(density, sigma, mode='constant')
+    return density
+
+save_path = './data/'
+os.makedirs(save_path, exist_ok=True)
+shape = (512, 512)
+res = 1
+n_max = 100
+n_div = 10
+
+for i in range(200):
+    print('generate', i) 
+    n = int(np.random.normal(n_max, n_div))
+    name = "img_{:03}.h5".format(i)
+    x = np.random.randint(shape[0], size=n)
+    y = np.random.randint(shape[1], size=n)
+
+    point = np.zeros(shape, dtype=np.float)
+    img = Image.new('L', shape)
+    draw = ImageDraw.Draw(img)
+    for i in range(n):
+        s = np.random.normal(20, 5)
+        if s < 5:
+            continue
+        draw.ellipse((y[i], x[i], y[i] + s, x[i] + s), fill = 'white')
+        s = np.random.normal(0, abs(s) / 2, size=2)
+        s = [0, 0]
+        nx = x[i] + int(s[0])
+        ny = y[i] + int(s[1])
+        if nx < 0: nx = 0
+        if ny < 0: ny = 0
+        if nx >= shape[0]: nx = shape[0] - 1
+        if ny >= shape[1]: ny = shape[1] - 1
+        point[nx, ny] = 1
+
+    d = gaussian_filter_density_fix(point)
+    with h5py.File(save_path + name, 'w') as hf:
+        # hf['point'] = point
+        hf['image'] = img
+        hf['density'] = d
+
+    """
+    plt.subplot(1, 2, 1)
+    plt.imshow(d)
+    plt.subplot(1, 2, 2)
+    plt.imshow(img)
+    plt.show()
+    """
---
+++ circle/train.py
@@ -0,0 +1,171 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import tqdm
+
+from models import *
+from dataset import Count, GaussianDataset
+
+try:
+    from termcolor import cprint
+except ImportError:
+    cprint = None
+
+
+output_dir = 'save/'
+dataset_name = 'gau'
+method = 'UNET'
+net = UNet(1, 1)
+loss_fn = CountLoss()
+loss_num_fn = nn.MSELoss()
+version = 'v2'
+start_epoches = 0
+epoches = 250
+lr = 1e-4
+weight_decay = 0
+patient_until = 0
+patient = 16
+rand_seed = 123
+batch = 5
+norm = 1000
+
+# init
+save_name = method + '_' + dataset_name + '_' + version
+if rand_seed is not None:
+    np.random.seed(rand_seed)
+    torch.manual_seed(rand_seed)
+    torch.cuda.manual_seed(rand_seed)
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+save_name = os.path.join(output_dir, save_name)
+
+# two
+# load net
+if start_epoches:
+    data = torch.load("{}.pt".format(save_name))
+    net.load_state_dict(data)
+
+print(net)
+net.cuda()
+
+# load dataset
+trainset = GaussianDataset(mode="train",
+                           norm=norm,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                           ]))
+validset = GaussianDataset(mode="valid",
+                           norm=norm,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                           ]))
+data_loader_trn = DataLoader(trainset,
+                             batch_size=batch,
+                             shuffle=True,
+                             num_workers=8)
+data_loader_val = DataLoader(validset,
+                             batch_size=batch,
+                             shuffle=True,
+                             num_workers=8)
+
+# optimizer
+optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, net.parameters()),
+        lr=lr,
+        weight_decay=weight_decay)
+
+# save data
+history = []
+best_mse = 1e99
+
+
+def log_print(text, color=None, on_color=None, attrs=None):
+    if cprint is not None:
+        cprint(text, color=color, on_color=on_color, attrs=attrs)
+    else:
+        print(text)
+
+
+for epoch in range(start_epoches + 1, epoches + 1):
+    train_count = Count(norm)
+    valid_count = Count(norm)
+    train_loss = 0
+
+    net.train()
+    for blob in tqdm.tqdm(data_loader_trn, ncols=100, leave=False):
+        gtmap = blob['dmap'].cuda()
+        dmap = net(blob['smap'].cuda())
+        # print(number)
+        optimizer.zero_grad()
+        # one
+        loss = loss_fn(dmap, gtmap)
+        loss.backward()
+        optimizer.step()
+        with torch.no_grad():
+            train_loss += loss
+            train_count.add(dmap, gtmap)
+
+    # calculate error on the validation dataset
+    net.eval()
+    with torch.no_grad():
+        for blob in tqdm.tqdm(data_loader_val, ncols=100, leave=False):
+            dmap = net(blob['smap'].cuda())
+            gtmap = blob['dmap']
+            valid_count.add(dmap, gtmap)
+
+    train_mae, train_mse = train_count.getResult()
+    valid_mae, valid_mse = valid_count.getResult()
+    history.append({'epoch': epoch,
+                    'train_mae': train_mae,
+                    'train_mse': train_mse,
+                    'valid_mae': valid_mae,
+                    'valid_mse': valid_mse,
+                    'loss': train_loss})
+    np.savez(save_name + '.npz', history=history)
+
+    # log
+    log_text = 'EPOCH: %d LOSS: %0.4f' % (epoch, train_loss)
+    log_text += '  TRAIN: MAE: %0.4f MSE: %0.4f' % (train_mae, train_mse)
+    log_text += '  VALID: MAE: %0.4f MSE: %0.4f' % (valid_mae, valid_mse)
+    log_print(log_text, color='green', attrs=['bold'])
+
+    # save best
+    if valid_mse < best_mse:
+        best_mae = valid_mae
+        best_mse = valid_mse
+        # best_model = '{}_{}.pt'.format(save_name, epoch)
+        best_model = '{}.pt'.format(save_name)
+        torch.save(net.state_dict(), best_model)
+        log_text = 'BEST MAE: %0.1f, BEST MSE: %0.1f, ' % (best_mae, best_mse)
+        log_text += 'BEST MODEL: %s' % (best_model,)
+        log_print(log_text, color='red', attrs=['bold'])
+        now_pat = patient
+    else:
+        if epoch > patient_until:
+            now_pat -= 1
+            if now_pat < 0:
+                break
+
+
+# plot
+history = history[:10]
+plt.suptitle(method + ' on Agar dataset')
+x = [h['epoch'] for h in history]
+plt.subplot(131)
+plt.plot(x, [h['valid_mae'] for h in history], label='valid_mae')
+plt.plot(x, [h['train_mae'] for h in history], label='train_mae')
+plt.legend()
+plt.subplot(132)
+plt.plot(x, [h['valid_mse'] for h in history], label='valid_mse')
+plt.plot(x, [h['train_mse'] for h in history], label='train_mse')
+plt.legend()
+plt.subplot(133)
+plt.plot(x, [h['loss'] for h in history], label='loss')
+plt.legend()
+plt.xlabel("epoch")
+plt.savefig(save_name + '.png')
+plt.show()
